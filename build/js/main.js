2e59cb1571e2886a864bb98aae42adf541e9d2a0
Toolnado <Toolnado@gmail.com>
2020-02-06 14:51:03 +0300

    main-block

--- build/js/main.js
+++ build/js/main.js
@@ -25,9 +25,15 @@ tpl:'<div class="fancybox-share"><h1>{{SHARE}}</h1><p><a class="fancybox-share__
 
 /* my scripts */
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("input[name='phone']").mask(" +7 (999) 999-99-99");
+});
 
-
-
+$(document).ready(function () {
+    $("#next-section-button").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 700);
+    });
 });
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,6 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("input[name='phone']").mask(" +7 (999) 999-99-99");
+});
 
-
-
+$(document).ready(function () {
+    $("#next-section-button").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 700);
+    });
 });
